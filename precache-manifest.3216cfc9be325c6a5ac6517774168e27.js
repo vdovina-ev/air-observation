502d744b228ecfa49082e7a4d2747f6f5871a44c
unknown <vdovina.ev.1@gmail.com>
2020-10-01 23:03:29 +0300

    Updates

---
+++ precache-manifest.3216cfc9be325c6a5ac6517774168e27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5205a8f48b1975656b300a82b4039d00",
+    "url": "/air-observation/index.html"
+  },
+  {
+    "revision": "be491c701895b763bff2",
+    "url": "/air-observation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1a7dfaf597d1330d8f8",
+    "url": "/air-observation/static/js/2.d2a66fa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/air-observation/static/js/2.d2a66fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be491c701895b763bff2",
+    "url": "/air-observation/static/js/main.cc5e85b5.chunk.js"
+  },
+  {
+    "revision": "b333cdfafff7a632d495",
+    "url": "/air-observation/static/js/runtime-main.86783cfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/air-observation/static/media/logo.5d5d9eef.svg"
+  }
+]);
